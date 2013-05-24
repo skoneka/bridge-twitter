bbccaa158ccd32058da04d695f7571b40dc87bcf
Simon Goumaz <simon@attentif.ch>
2013-05-24 16:39:18 +0200

    Added dev environment setup script (calls shared base setup script including nvm, node, JSHint, pre-commit hook, initial npm install)

--- README.md
+++ README.md
@@ -2,13 +2,10 @@
 
 Node.JS server handling Twitter to Pryv integration. Manages the forwarding of selected user tweets to their Pryv account, including authorization on Twitter and Pryv, and configuration.
 
-## Installation
 
-To get the external dependencies, run:
+## Setting up the development environment on a Mac
 
-    $npm install
-
-N.B. Mongodb needs to be installed locally
+Read, then execute `./scripts/setup-dev-environment.bash`. It will check for Mongo DB in the parent folder and install it if necessary.
 
 
 ## Usage
@@ -180,11 +177,6 @@ Provides the settings of all users in the db.
 To run the tests, simply execute the makefile:
 
 	$make
-
-
-## Comments
-
-The ntwitter library has been slightly modified so it has been left in the node_modules folder.
 
 
 ## License
---
+++ scripts/setup-dev-environment.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Sets up the dev environment on a 64-bit OSX system.
+# Re-run to update e.g. the node version (from the new default) or the JSHint config (from the master).
+
+# working dir fix
+SCRIPT_FOLDER=$(cd $(dirname "$0"); pwd)
+cd $SCRIPT_FOLDER/.. # root
+
+export NODE_VERSION=v0.8.23
+
+export DATA_FOLDER=$SCRIPT_FOLDER/../..
+export LOGS_FOLDER=${DATA_FOLDER}/logs
+
+export MONGO_NAME=mongodb-osx-x86_64-2.4.1
+export MONGO_DL_BASE_URL=http://fastdl.mongodb.org/osx
+export MONGO_BASE_FOLDER=$DATA_FOLDER
+export MONGO_DATA_FOLDER=$DATA_FOLDER/mongodb-data
+
+# base
+
+curl -s https://raw.github.com/pryv/dev-scripts/master/setup-repo-copy.js-node.bash | bash
+EXIT_CODE=$?
+if [[ ${EXIT_CODE} -ne 0 ]]; then
+  echo ""
+  echo "Error: base setup failed. Setup aborted."
+  echo ""
+  exit $((${EXIT_CODE}))
+fi
+
+# file structure
+
+mkdir -p $LOGS_FOLDER
+
+# database
+
+curl -s https://raw.github.com/pryv/dev-scripts/master/setup-mongodb.bash | bash
+EXIT_CODE=$?
+if [[ ${EXIT_CODE} -ne 0 ]]; then
+  echo ""
+  echo "Error setting up database; setup aborted"
+  echo ""
+  exit ${EXIT_CODE}
+fi
+
+
+echo ""
+echo "Setup complete!"
+echo ""
